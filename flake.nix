e2a007a0dfc353bde935b5a5ed66096859ae6b2b
boticelli <kandelakitina@gmail.com>
2025-01-27 11:34:57 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,9 @@
 {
-  inputs.flakelight.url = "github:nix-community/flakelight";
-  outputs = { flakelight, ... }:
+  inputs = {
+    flakelight.url = "github:nix-community/flakelight";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { flakelight, nixpkgs, ... }:
     flakelight ./. {
       devShell.packages = pkgs:
         with pkgs;
